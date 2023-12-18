f68aa5455ef776925fac7376129634737c3cde6b
Marcin Michalski <mkamichalski@gmail.com>
2023-12-18 22:44:39 +0100

    0.0.0.1
    Tailwind styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,122 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { style } from "./assets/Styling";
 
 function App() {
-  
+  const [mySites, setMySites] = useState([]);
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputComment, setInputComment] = useState("");
+
+  useEffect(() => {
+    const storage = chrome?.storage?.local;
+    // const storage = chrome?.storage?.local || browser?.storage?.local;
+
+    if (!storage) {
+      console.error("WebExtensions storage API not supported");
+      return;
+    }
+
+    storage.get("mySites", (result) => {
+      const sitesFromStorage = result.mySites;
+      if (sitesFromStorage) {
+        setMySites(sitesFromStorage);
+      }
+    });
+  }, []);
+
+  const saveTab = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const newSite = {
+        title: inputTitle || tabs[0].title,
+        comment: inputComment,
+        url: tabs[0].url,
+      };
+
+      setMySites([...mySites, newSite]);
+
+      const storage = chrome?.storage?.local || browser?.storage?.local;
+      if (storage) {
+        storage.set({ mySites: [...mySites, newSite] });
+      }
+
+      setInputTitle("");
+      setInputComment("");
+    });
+  };
+
+  const deleteSite = (index) => {
+    const updatedSites = [...mySites];
+    updatedSites.splice(index, 1);
+
+    setMySites(updatedSites);
+
+    const storage = chrome?.storage?.local || browser?.storage?.local;
+    if (storage) {
+      storage.set({ mySites: updatedSites });
+    }
+  };
 
   return (
-    <>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <div className={style.logo}>Tabs</div>
+      <input
+      className={style.input}
+        type='text'
+        id='input-title'
+        placeholder='custom title'
+        maxLength='35'
+        required
+        value={inputTitle}
+        onChange={(e) => setInputTitle(e.target.value)}
+      />
+      <textarea
+      className={style.textarea}
+        rows='4'
+        id='input-comment'
+        placeholder='your comment...'
+        maxLength='175'
+        value={inputComment}
+        onChange={(e) => setInputComment(e.target.value)} />
       
-    </>
-  )
+        <button onClick={saveTab} id='save-btn' className={style.savebutton}>
+          Save Tab
+        </button>
+      
+      <ul>
+        {mySites.map((site, index) => (
+          <li key={index}>
+            <div>
+              <button onClick={() => deleteSite(index)}>Delete</button>
+            </div>
+            <a target='_blank' href={site.url}>
+              {site.title}
+            </a>
+            <p>{site.comment}</p>
+          </li>
+        ))}
+        <li>
+            <a target='_blank' href="#" className={style.title}>
+              Tutuł strony
+            </a>
+            <p className={style.comment}>Opis strony mideidjei custom</p>
+            <div>
+              <button onClick={() => deleteSite(index)}>Delete</button>
+            </div>
+          </li>
+          <li>
+            <a target='_blank' href="#" className={style.title}>
+              Druga strona
+            </a>
+            <p className={style.comment}>Inny opis</p>
+            <div>
+              <button onClick={() => deleteSite(index)}>Delete</button>
+            </div>
+          </li>
+      </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/Styling.jsx
@@ -0,0 +1,11 @@
+export const style = {
+    bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#ccc] to-[#48a4ff]`,
+    container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shsdow-xl p-4`,
+    logo: `text-3xl font-bold text-gray-800 p-2`,
+    input: `border p-2 w-full text-xl`,
+    textarea: `w-full p-2`,
+    savebutton: ``,
+    title: ``,
+    comment: ``,
+
+  }
