ebf758531a29d7bbdf41411114bb86586bd259fb
Marcin Michalski <mkamichalski@gmail.com>
2023-12-19 21:42:31 +0100

    0.0.2.0
    Styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { style } from "./assets/Styling";
+import { RxCross2 } from "react-icons/rx";
 
 function App() {
   const [mySites, setMySites] = useState([]);
@@ -59,53 +60,61 @@ function App() {
   return (
     <div className={style.bg}>
       <div className={style.container}>
-        <div className={style.logo}>Tabs</div>
-      <input
-      className={style.input}
-        type='text'
-        id='input-title'
-        placeholder='custom title'
-        maxLength='35'
-        required
-        value={inputTitle}
-        onChange={(e) => setInputTitle(e.target.value)}
-      />
-      <textarea
-      className={style.textarea}
-        rows='2'
-        id='input-comment'
-        placeholder='additional comment'
-        maxLength='125'
-        value={inputComment}
-        onChange={(e) => setInputComment(e.target.value)} />
-      
+        <div className={style.logo}>
+          <p className={style.logoText}>TabMinder</p>
+        </div>
+        <input
+          className={style.input}
+          type='text'
+          id='input-title'
+          placeholder='custom title'
+          maxLength='35'
+          required
+          value={inputTitle}
+          onChange={(e) => setInputTitle(e.target.value)}
+        />
+        <input
+          className={style.input}
+          id='input-comment'
+          placeholder='additional comment'
+          maxLength='150'
+          value={inputComment}
+          onChange={(e) => setInputComment(e.target.value)}
+        />
+
         <button onClick={saveTab} id='save-btn' className={style.savebutton}>
-          Save Tab
+          <span>+</span> Add Tab
         </button>
-      
-      <ul>
-        {mySites.map((site, index) => (
-          <li key={index}>
-            <div>
-              <button onClick={() => deleteSite(index)}>Delete</button>
-            </div>
-            <a target='_blank' href={site.url}>
-              {site.title}
-            </a>
-            <p>{site.comment}</p>
-          </li>
-        ))}
-        <li>
+
+        <ul>
+          {mySites.map((site, index) => (
+            <li key={index}>
+              <div className={style.tabContainer}>
+                <div className={style.tabtext}>
+                  <a target='_blank' href={site.url} className={style.tabTitle}>
+                    {site.title}
+                  </a>
+                  <p className={style.tabComment}>{site.comment}</p>
+                </div>
+                <div className={style.tabDelete}>
+                  <button onClick={() => deleteSite(index)}>
+                    <RxCross2 />
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+          {/* <li>
           <div className={style.tabContainer}>
           <div className={style.tabtext}>
             <a target='_blank' href="#" className={style.tabTitle}>
-              Tutuł strony
+              dev test title1
             </a>
             
-            <p className={style.tabComment}>Opis strony mideidjei custom</p>
+            <p className={style.tabComment}>dev test 1 desrptn</p>
             </div>
             <div className={style.tabDelete}>
-              <button onClick={() => deleteSite(index)}>Delete</button>
+              <button onClick={() => deleteSite(index)}><RxCross2 /></button>
             </div>
             </div>
           </li>
@@ -114,17 +123,17 @@ function App() {
           <div className={style.tabContainer}>
           <div className={style.tabtext}>
             <a target='_blank' href="#" className={style.tabTitle}>
-              Tutuł strony
+              dev test title2
             </a>
             
-            <p className={style.tabComment}>Opis strony mideidjei custom</p>
+            <p className={style.tabComment}>dev test 2 desrptn</p>
             </div>
             <div className={style.tabDelete}>
-              <button onClick={() => deleteSite(index)}>Delete</button>
+              <button onClick={() => deleteSite(index)}><RxCross2 /></button>
             </div>
             </div>
-          </li>
-      </ul>
+          </li> */}
+        </ul>
       </div>
     </div>
   );
--- src/assets/Styling.jsx
+++ src/assets/Styling.jsx
@@ -1,13 +1,15 @@
 export const style = {
-    bg: `h-screen w-screen p-4 bg-[#fafafa]`,
-    container: `max-w-[450px] w-full m-auto shsdow-xl p-4`,
-    logo: `text-2xl font-bold text-[#174873] p-2 mb-2`,
-    input: `bg-transparent p-2 w-full text-xs text-[#174873] border-b-[1px] border-[#cccccc] mb-2 transition duration-200 focus:border-[#174873] focus:outline-none placeholder:text-xs`,
-    textarea: `bg-transparent w-full text-xs text-[#174873] border-b-[1px] border-[#cccccc] p-2 mb-2 resize-none transition duration-200 focus:border-[#174873] focus:outline-none placeholder:text-xs placeholder:pt-4`,
-    savebutton: `border-2 p-1 mb-3`,
-    tabContainer: `bg-[#FFFFFF] flex flex-row justify-between shadow-sm rounded-md p-3 mb-3 border-[1px] hover:border-[#cccccc] transition duration-200`,
-    tabTitle: `font-bold text-[#295FA6]`,
-    tabComment: `text-sm text-[#444444]`,
-    tabDelete: `text-[#BF2424]`
+    bg: `h-screen w-screen p-4 bg-[radial-gradient(120%_70%_at_10%_20%,rgba(40,40,40)_0%,rgba(25,25,25)_100%)]`,
+    container: `p-5 w-full m-auto bg-[#121212] rounded-md`,
+    logo: `font-outfit text-4xl font-bold p-2 mb-2`,
+    logoText: `text-transparent bg-clip-text bg-gradient-to-r from-[#F2B705] to-[#F24141] max-w-[220px]`,
+    input: `bg-transparent p-2 w-full text-xs text-[#cccccc] border-b-[1px] border-[#333333] mb-5 transition duration-200 focus:border-[#555555] focus:outline-none placeholder:text-xs`,
+    savebutton: `relative border-[1px] rounded-md border-[#333333] text-sm text-[#F2B705] py-1 w-[120px] mb-8 mt-3 hover:border-[#F2B705] transition duration-200`,
+    tabContainer: `relative bg-[#222222] flex flex-row justify-between  rounded-md p-3 mb-[10px] hover:bg-[#2a2a2a] hover:scale-[1.02] hover:shadow-md transition duration-200`,
+    tabTitle: `font-bold text-[#F2B705]`,
+    tabComment: `text-sm text-[#eeeeee]`,
+    tabDelete: `text-[#F24141] text-xs`,
+    iconCross: ``,
+    
 
   }
